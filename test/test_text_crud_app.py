7f8a08a7440d2a08cefb56e76250ebc0361ffc13
HuzefaC <huzefachabukswar@gmail.com>
2022-04-01 20:42:28 +0530

    Added test to check if the page heading is words

--- test/test_text_crud_app.py
+++ test/test_text_crud_app.py
@@ -1,5 +1,6 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 PATH = "../geckodriver.exe"
 
@@ -14,6 +15,13 @@ class TestTextCrudApp(unittest.TestCase):
 
     def test_browser_title_contains_app_name(self):
         self.assertIn('Text Crud', self.driver.title)
+
+    def test_page_heading_is_words(self):
+        heading = self._find(val='heading')
+        self.assertEqual(heading, 'Words')
+
+    def _find(self, val):
+        return self.driver.find_element(By.CSS_SELECTOR, f'[data-test-id="{val}"]')
 
 
 if __name__ == '__main__':
