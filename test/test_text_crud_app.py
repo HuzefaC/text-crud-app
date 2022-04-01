c9192f091928c26074f5f10ff598290d77783a7a
HuzefaC <huzefachabukswar@gmail.com>
2022-04-01 20:09:41 +0530

    Added test to test browser title

--- test/test_text_crud_app.py
+++ test/test_text_crud_app.py
@@ -1,5 +1,20 @@
 import unittest
+from selenium import webdriver
+
+PATH = "../geckodriver.exe"
 
 
-class TextCrudApp(unittest.TestCase):
-    pass
+class TestTextCrudApp(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Firefox(executable_path=PATH)
+        self.driver.get('https://localhost:5000')
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_browser_title_contains_app_name(self):
+        self.assertIn('Text Crud', self.driver.title)
+
+
+if __name__ == '__main__':
+    unittest.main()
